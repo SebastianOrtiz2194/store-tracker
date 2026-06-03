91c75eaec384ae179bedf3b1d17562c2d76d5487
SebastianOrtiz2194 <sebastian_2194@hotmail.com>
2026-06-03 15:27:27 +0900

    feat(modulith): add spring modulith boundary verification test

---
+++ src/test/java/com/store/tracker/ModularityTests.java
@@ -0,0 +1,12 @@
+package com.store.tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.modulith.core.ApplicationModules;
+
+class ModularityTests {
+
+    @Test
+    void verifiesModularStructure() {
+        ApplicationModules.of(TrackerApplication.class).verify();
+    }
+}
